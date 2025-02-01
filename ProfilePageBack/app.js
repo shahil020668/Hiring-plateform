8ee12c2ce1de8bb5c80d708399a861971d682ad6
zynox <mohammadshahil0068@gmail.com>
2025-02-01 10:22:46 +0530

    commited

--- ProfilePageBack/app.js
+++ ProfilePageBack/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // app.get('/',(req, res) =>{
-//     res.send('Hello World!')
+
 // })
 
 app.use('/profile',profileRouter)
